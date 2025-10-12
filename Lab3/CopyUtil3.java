cf603aac143bcfc162e48f48e91d01c7df428956
zqld <124332618+zqld@users.noreply.github.com>
2025-10-13 01:33:09 +0900

    LAB3.Task2
    Задание #2
    В последнем двупоточном решении читатель постоянно создает новые byte[]-буфера, передает их писателю, а тот отправляет на съедение GC. Создайте отдельную обратную очередь пустых буферов от писателя к читателю.

--- Lab3/CopyUtil3.java
+++ Lab3/CopyUtil3.java
@@ -13,48 +13,49 @@ public class CopyUtil3 {
     public static void copy(final InputStream src, final OutputStream dst) throws IOException {
         // reader-to-writer byte[]-channel 
         final BlockingQueue<byte[]> buffer = new ArrayBlockingQueue<>(64); // буфер в виде блок очереди длиной 64 эл-та (эл-т byte[] длиной 128)
+        final BlockingQueue<byte[]> emptyBuffer = new ArrayBlockingQueue<>(64); // пустой буфер, которые будет использоваться еще раз
         // exception-channel from reader/writer threads?
         // Канал для передачи необработанного исключения из ThreadGroup.uncaughtException в поток, который вызовет copy (главный поток), чтобы в конце бросить IOException с причиной.
         final AtomicReference<Throwable> ex = new AtomicReference<>(); // атомарные операции для ссылок на объекты (вместо synchronized)
-        // final ThreadGroup group = new ThreadGroup("read-write") { // управление потоками как единым целым
-        //     public void uncaughtException(Thread t, Throwable e) {ex.set(e);} // ловим исплючение, которое может "выпрыгнуть" (кроме тех, которые пойманы нормальным путем)
-        // };
+
+        final ThreadGroup group = new ThreadGroup("read-write") { // управление потоками как единым целым
+            public void uncaughtException(Thread t, Throwable e) {ex.set(e);} // ловим исплючение, которое может "выпрыгнуть" (кроме тех, которые пойманы нормальным путем)
+        };
+
         // reader from 'src' - поток для чтения
-        Thread reader = new Thread(() -> { //group
+        Thread reader = new Thread(group, () -> {
             try (InputStream src0 = src) {              // 'src0' for auto-closing (try with resouerces)
                 while (true) {
-                    byte[] data = new byte[128];        // new data buffer
+                    byte[] data = emptyBuffer.poll(); // извлекает и удаляет элемент из начала очереди, возвращая null если очередь пуста
+                    if(data == null) data = new byte[128]; // проверяем, если нет свободного буфера, создает новый
+                    //byte[] data = new byte[128];        // new data buffer
                     int count = src.read(data, 1, 127); // read up to 127 bytes (оставляет data[0] для метаданных)
                     data[0] = (byte) count;             // 0-byte is length-field (помещаем в 0 байт длину)
                     buffer.put(data);                   // send to writer (помещается в очередь - если полная очередь, ждет появления места - поток чтения ждет)
                     if (count == -1) {break;}           // src empty
                 }
-            } catch (Exception e) {                     //group.interrupt(); interrupt writer (прерываем всю группу, исключение не заносится в ex)
-                try {
-                    // если ошибка — кладём маркер конца (-1)
-                    byte[] end = new byte[1];
-                    end[0] = -1;
-                    buffer.put(end);
-                } catch (InterruptedException ignored) {}
-            }
+            } catch (Exception e) {group.interrupt();}  // interrupt writer (прерываем всю группу, исключение не заносится в ex)
         });
         reader.start();
+
         // writer to 'dst' - поток для записи
-        // Thread writer = new Thread(group, () -> {
-        try (OutputStream dst0 = dst) {      // 'dst0' for auto-closing (try with resouerces)
-            while (true) {
-                byte[] data = buffer.take(); // get new data from reader (ждет, пока очередь пуста, пока не появятся новые данные)
-                if (data[0] == -1) {break;}  // its last data (прерывание на последнюю дату)
-                dst.write(data, 1, data[0]); // записывает data[0] байт начиная с позиции 1
-            }
-        } catch (Exception e) {ex.set(e);}  // group.interrupt(); interrupt writer (прерываем всю группу, исключение не заносится в ex)
-        // });
-        // writer.start();
-        // // wait to complete read/write operations (главный поток ждёт завершения)
-        // try {
-        //     reader.join(); // wait for reader
-        //     writer.join(); // wait for writer
-        // } catch (InterruptedException e) {throw new IOException(e);}
+        Thread writer = new Thread(group, () -> {
+            try (OutputStream dst0 = dst) {      // 'dst0' for auto-closing (try with resouerces)
+                while (true) {
+                    byte[] data = buffer.take(); // get new data from reader (ждет, пока очередь пуста, пока не появятся новые данные)
+                    if (data[0] == -1) {break;}  // its last data (прерывание на последнюю дату)
+                    dst.write(data, 1, data[0]); // записывает data[0] байт начиная с позиции 1
+
+                    emptyBuffer.offer(data); // добавляем в очередь без блокировок (если очередь не заполнена), возвращает true или false
+                }
+            } catch (Exception e) {ex.set(e); group.interrupt();}  // interrupt writer (прерываем всю группу, исключение не заносится в ex)
+        });
+        writer.start();
+        // wait to complete read/write operations (главный поток ждёт завершения)
+        try {
+            reader.join(); // wait for reader
+            writer.join(); // wait for writer
+        } catch (InterruptedException e) {throw new IOException(e);}
         if (ex.get() != null) {throw new IOException(ex.get());} // обработка ошибки из ex
     }
 }
