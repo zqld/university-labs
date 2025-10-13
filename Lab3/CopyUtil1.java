d59b563bc0db2f2cde904bb1c7ed614c6b88521b
zqld <124332618+zqld@users.noreply.github.com>
2025-10-13 09:33:39 +0900

    LAB3.Task3
    Задание #3
    Во всех трех примерах кода мы реализовывали передачу данных от одного читателя — одному писателю. Реализуйте многопоточное решение передачи данных от одного читателя — многим писателям. Все писатели получают идентичные данные. Читатель и писатели работают каждый в своем отдельном потоке. Не создавайте отдельные копии данных для каждого писателя — пусть писатели читают из одних на всех буферов, но храните эти буфера одновременно в разных очередях (от читателя к каждому писателю тянется отдельная очередь).
    
    import java.io.IOException;
    import java.io.InputStream;
    import java.io.OutputStream;
    
    public class CopyUtil {
        public static void copy(InputStream src, OutputStream ... dst) throws IOException {
            // some code
        }
    }

--- Lab3/CopyUtil1.java
+++ Lab3/CopyUtil1.java
@@ -6,11 +6,17 @@ import java.io.OutputStream;
 //Копирование происходит в потоке вызвавшем метод copy(...)
 
 public class CopyUtil1 {
-    public static void copy(InputStream src, OutputStream dst)throws IOException{
-        try (InputStream src0 = src; OutputStream dst0 = dst) { // try with resources (закрывает в обратном порядке)
+    public static void copy(InputStream src, OutputStream... dst) throws IOException {
+        try (InputStream src0 = src) { // try with resources (закрывает в обратном порядке)
+            // поток закроется сам, а вот выходные потоки закрываем после копирования (потому что непонятно сколько их)
             int b; // read вовзращает инт и -1 при конце потока
-            while ((b = src.read()) != -1) { //-1 = конец потока
-                dst.write(b); // запись одиночного байта (записывает младший байт)
+            while ((b = src0.read()) != -1) { //-1 = конец потока
+                for (OutputStream d : dst) { // для всех выходных потоков
+                    d.write(b); // запись одиночного байта (записывает младший байт)
+                }
+            }
+            for (OutputStream d : dst) {
+                d.close(); // закрываем все выходные потоки
             }
         }
     }
--- Lab3/CopyUtil2.java
+++ Lab3/CopyUtil2.java
@@ -4,14 +4,18 @@ import java.io.OutputStream;
 //Это реализация однопоточного копировальщика массивами из InputStream в OutputStream. 
 //Копирование происходит в потоке вызвавшем метод copy(...)
 
-
 public class CopyUtil2 {
-    public static void copy(InputStream src, OutputStream dst)throws IOException{
+    public static void copy(InputStream src, OutputStream... dst) throws IOException {
         byte[] buff = new byte[128]; // буфер 128 байт для чтения блоками
-        try (InputStream src0 = src; OutputStream dst0 = dst) {
+        try (InputStream src0 = src) { 
             int count;  // read вовзращает инт и -1 при конце потока
-            while ((count = src.read(buff)) != -1) { // заполняет буфер полностью и возвращает кол-во прочитаных байт (-1 если конец)
-                dst.write(buff, 0, count); // запись только count байт из начала массива buff(0 - начало массива)
+            while ((count = src0.read(buff)) != -1) {// заполняет буфер полностью и возвращает кол-во прочитаных байт (-1 если конец)
+                for (OutputStream d : dst) {
+                    d.write(buff, 0, count); // запись только count байт из начала массива buff(0 - начало массива) (для всех потоков)
+                }
+            }
+            for (OutputStream d : dst) {
+                d.close(); // закрываем всех потоки
             }
         }
     }
